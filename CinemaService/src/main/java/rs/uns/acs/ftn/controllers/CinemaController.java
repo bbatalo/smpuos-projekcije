8097786d8b1a822909ff0b96c7afc32f1b2f0449
Bojan Batalo <bbatalo@hotmail.com>
2018-01-06 16:44:29 +0100

    Minor change

--- CinemaService/src/main/java/rs/uns/acs/ftn/controllers/CinemaController.java
+++ CinemaService/src/main/java/rs/uns/acs/ftn/controllers/CinemaController.java
@@ -4,11 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import rs.uns.acs.ftn.dto.CinemaAuthDTO;
@@ -58,5 +60,11 @@ public class CinemaController extends AbstractRESTController {
 		m.put("success", true);
 		m.put("created", created);
 		return m;
+	}
+	
+	@FeignClient("user-service")//the application.name for the user service
+	public interface UserServiceClient {
+		@RequestMapping(value = "users/checkUser", method = RequestMethod.GET)// the endpoint which will be balanced over
+		Boolean checkUser(@RequestParam(name = "userId") String userId);// the method specification must be the same as for users/checkUser
 	}
 }
