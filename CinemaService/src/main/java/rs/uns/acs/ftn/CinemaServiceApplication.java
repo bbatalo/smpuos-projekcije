3025535a697337e6b603ba94080ec0470de0467d
Bojan Batalo <bbatalo@hotmail.com>
2018-01-06 11:09:01 +0100

    Add required dependencies to start the service

--- CinemaService/src/main/java/rs/uns/acs/ftn/CinemaServiceApplication.java
+++ CinemaService/src/main/java/rs/uns/acs/ftn/CinemaServiceApplication.java
@@ -1,12 +1,17 @@
 package rs.uns.acs.ftn;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableAutoConfiguration
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CinemaServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CinemaServiceApplication.class, args);
+		SpringApplication app = new SpringApplication(CinemaServiceApplication.class);
+		app.run(args);
 	}
 }
