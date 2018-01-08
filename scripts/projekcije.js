87bab656e5c3ba63c8e4e827f8e2f347694a2771
Milos Sidji <milos.sidji@gmail.com>
2018-01-08 23:55:09 +0100

    Izmenjena skripta projekcije za unos u bazu (realniji primeri).

--- scripts/projekcije.js
+++ scripts/projekcije.js
@@ -1,21 +1,38 @@
 var insertDefaultDataProjekcije = function(db){
     db.projekcije.insert([
         {
-            "date" : ISODate("1991-01-01T00:00:00.000Z"),
-            "type": "REGULAR",
-            "status": "ACTIVE",
-            "cinemaId": 1,
-            "hallId": 1,
-            "movieId": 1
+            "date" : ISODate("2018-01-10T00:10:00.000Z"),
+            "type" : "PREMIERE",
+            "status" : "INACTIVE",
+            "cinemaId" : "1",
+			"cinemaName" : "Cinema SRB",
+            "hallId" : "11",
+			"hallLabel" : "Sala11",
+            "movieId" : "100",
+			"movieName" : "Биће боље - aб"
 		},
         {
-            "date" : ISODate("1991-01-01T00:00:00.000Z"),
-            "type": "PREMIERE",
-            "status": "ACTIVE",
-            "cinemaId": 2,
-            "hallId": 1,
-            "movieId": 2
-        }
+            "date" : ISODate("2018-01-13T13:00:00.000Z"),
+            "type" : "PREMIERE",
+            "status" : "ACTIVE",
+            "cinemaId" : "2",
+			"cinemaName" : "Cinema SRB",
+            "hallId" : "22",
+			"hallLabel" : "Sala22",
+            "movieId" : "200",
+			"movieName" : "Brže, jače, bolje 4"
+        },
+        {
+			"date" : ISODate("2018-01-15T15:00:00.000Z"),
+            "type" : "REGULAR",
+            "status" : "ACTIVE",
+            "cinemaId" : "5a5169492e1dcff9ec8f1b70",
+			"cinemaName" : "Arena",
+            "hallId" : "5a5169492e1dcff9ec8f1b6c",
+			"hallLabel" : "KLASIKANERSKA",
+            "movieId" : "5a5169492e1dcff9ec8f1b68",
+			"movieName" : "The Notebook"
+        }		
     ]);
 
 };
